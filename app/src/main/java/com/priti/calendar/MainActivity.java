eb9ce36de0e34e2e589bb51355dd5c6eafb40a9e
pritii <pritiky910@gmail.com>
2023-04-15 20:16:06 +0530

    calendar

---
+++ app/src/main/java/com/priti/calendar/MainActivity.java
@@ -0,0 +1,41 @@
+package com.priti.calendar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    DatePicker datePicker;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        datePicker = findViewById(R.id.date);
+        textView = findViewById(R.id.textView);
+
+        Calendar calendar = Calendar.getInstance();
+
+        datePicker.init(
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH),
+                new DatePicker.OnDateChangedListener() {
+                    @Override
+                    public void onDateChanged(DatePicker datePicker, int year, int month, int day) {
+                        textView.setText("you selected: " + day +"-" +month + "-" +year);
+
+                    }
+                }
+        );
+    }
+
+
+}
